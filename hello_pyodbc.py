91afcb1e91e168a3331493976ebd97abcf33a172
ecelg <107917928+ecelg@users.noreply.github.com>
2022-06-21 21:56:28 +0800

    Update hello_pyodbc.py

--- hello_pyodbc.py
+++ hello_pyodbc.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-connection=pyodbc.connect("DRIVER={InterSystems IRIS ODBC35};SERVER=k8s-64ea0e19-a8475938-131e349afd-7309e19823b7b6b4.elb.us-east-1.amazonaws.com;PORT=1972;DATABASE=USER;UID=SQLAdmin;PWD=!2021Ischk")
+connection=pyodbc.connect("DRIVER={InterSystems IRIS ODBC35};SERVER=k8s-64ea0e19-a8475938-131e349afd-7309e19823b7b6b4.elb.us-east-1.amazonaws.com;PORT=1972;DATABASE=USER;UID=SQLAdmin;PWD=pass")
 print("connected to IRIS SQLCloud")
 cursor=connection.cursor()
 z="select * from patient where PatientID between ? and ?"
